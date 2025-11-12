1dcdf649d21a14e1bdcd19380ee3cf5b73fe9a0a
Proto <Humayoon04syed@gmail.com>
2025-11-13 00:23:48 +0500

    ongoing

--- day04-setup.sql
+++ day04-setup.sql
@@ -14,3 +14,7 @@ UPDATE blogs.users SET age = 25 WHERE username = 'valid_email' RETURNING usernam
 
 -- Test invalid age (too young)
 UPDATE blogs.users SET age = 12 WHERE username = 'valid_email';
+
+-- RegEx wildcard so insteda of [A-Za-z0-9_]+ we can simply write \w+ and catpuring group ( \w+){0,3} where it captures a space followed by a word 0-3 times; 
+alter table blogs.users add constraint users_username_format check (username ~* '^\w+( \w+){0,3}$');
+update blogs.users set username = 'Queen Elizabeth of England' where id = 19;
